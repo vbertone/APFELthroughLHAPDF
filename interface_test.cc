1deca1eb604843ebf986856122ac02237ddf7dbe
Valerio Bertone <valerio.bertone@cern.ch>
2022-12-31 11:03:49 +0100

    Fixing code to make it work with newer versions of LHAPDF

--- interface_test.cc
+++ interface_test.cc
@@ -17,51 +17,72 @@ LHAPDF::PDF* mkPDF(apfel::InitialiseEvolution const& ev)
   const std::map<double, std::map<int, apfel::LHKnotArray>> ka = ev.KnotArray();
 
   // Knot array to be fed to LHAPDF
-  std::map<double, LHAPDF::KnotArrayNF> knotarrays;
+  LHAPDF::KnotArray data;
 
-  // Vector of flavour indices to be filled
-  std::vector<int> flavors;
-
-  // Vectors needed to construct the LHAPDF::AlphaS object
-  std::vector<double> q2as;
-  std::vector<double> als;
-
-  // Loop over Q subgrids
+  // Loop over Q subgrids to accumulate x-grid, q2-grid, and flavour
+  // IDs.
   for (auto const& sg : ka)
     {
-      // NF-dependent knot array for a single subgrid in Q
-      LHAPDF::KnotArrayNF kaNF;
+      // Get x-grid from first flavour of the first subgrid
+      if (data.xs().empty())
+	data.setxknots() = sg.second.begin()->second.xs;
 
-      // Loop over flavour IDs and fill in the know array (use 21 for
-      // the gluon).
-      for (auto const& id : sg.second)
-	kaNF.set_pid((id.first == 0 ? 21 : id.first), LHAPDF::KnotArray1F{id.second.xs, id.second.q2s, id.second.xfs});
+      // Accumulate q2 grid using the first flavour
+      data.setq2knots().insert(data.setq2knots().end(), sg.second.begin()->second.q2s.begin(), sg.second.begin()->second.q2s.end());
 
-      // Fill in the Q2 grid for alphas using the first element of the
-      // map. Remove the last element but do not do it for the last
-      // subgrid (this is why pop_back appears before).
-      if (!q2as.empty())
-	q2as.pop_back();
-      q2as.insert(q2as.end(), sg.second.begin()->second.q2s.begin(), sg.second.begin()->second.q2s.end());
-
-      // Fill in the flavour ID vector
-      if (flavors.empty())
+      // Fill in the flavour ID vector (use 21 for the gluon)
+      if (data.setPids().empty())
 	for (auto const& id : sg.second)
-	  flavors.push_back((id.first == 0 ? 21 : id.first));
+	  data.setPids().push_back((id.first == 0 ? 21 : id.first));
+    }
 
-      // Fill in the LHAPDF:KnotArrayNF
-      knotarrays.insert({sg.first, kaNF});
+  // Fill the knots of Knotarray
+  data.setShape() = std::vector<size_t>{data.xs().size(), data.q2s().size(), data.setPids().size()};
+  data.fillLogKnots();
+  data.initPidLookup();
+
+  // Set size of data vector
+  data.setGrid().resize(data.shape(0) * data.shape(1) * data.shape(2), 0.);
+
+  // Loop over Q subgrids to accumulate data
+  int nqprev = 0;
+  for (auto const& sg : ka)
+    {
+      // Initialise flavour index
+      int ifl = 0;
+
+      // Get q2-subgrid size
+      const int q2size = (int) sg.second.begin()->second.xfs.size() / data.shape(0);
+
+      // Loop over flavours
+      for (auto const& id : sg.second)
+	{
+	  // Get vector of distributions
+	  const std::vector<double> f = id.second.xfs;
+
+	  // Loop over x-grid
+	  for (int ix = 0; ix < (int) data.shape(0); ix++)
+	    {
+	      int iq = nqprev;
+	      // Loop over q2-sugrid
+	      for (int iqs = 0; iqs < q2size; iqs++)
+		data.setGrid()[ix * data.shape(2) * data.shape(1) + iq++ * data.shape(2) + ifl] = f[ix * q2size + iqs];
+	    }
+	  ifl++;
+	}
+      nqprev += q2size;
     }
 
   // Fill in alpha_s vector
-  for (auto const& q2 : q2as)
+  std::vector<double> als;
+  for (auto const& q2 : data.q2s())
     als.push_back(ev.Alphas(sqrt(q2)));
 
   // Define an LHAPDF GridPDF object
   LHAPDF::GridPDF* dist = new LHAPDF::GridPDF{};
 
   // Pass knot arrays to LHAPDF
-  dist->knotarrays() = knotarrays;
+  dist->Data() = data;
 
   // Set interpolator
   dist->setInterpolator((std::string) "logcubic");
@@ -70,11 +91,11 @@ LHAPDF::PDF* mkPDF(apfel::InitialiseEvolution const& ev)
   dist->setExtrapolator((std::string) "continuation");
 
   // Set flavours
-  dist->setFlavors(flavors);
+  dist->setFlavors(data.setPids());
 
   // Set alpha_s
   LHAPDF::AlphaS_Ipol* as = new LHAPDF::AlphaS_Ipol{};
-  as->setQ2Values(q2as);
+  as->setQ2Values(data.q2s());
   as->setAlphaSValues(als);
   dist->setAlphaS(as);
   
